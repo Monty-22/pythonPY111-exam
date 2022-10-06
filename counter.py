9ed93d0fc0a9a5b03e119dc5cb4db236e6272dbf
alecher <sasha-cherba@yandex.ru>
2022-10-06 20:53:22 +0300

    counter done

--- counter.py
+++ counter.py
@@ -1,1 +1,24 @@
+def counter(bytes_, players):
+    if len(players) == 1:
+        print(f"победитель - {players[0]}")
+        return players
+    i = 0
+    len_players = len(players)
+    print(f" Len => {len_players}")
+    for word in bytes_:
+        if i > len_players - 1:
+            i = 0
+        print(f"{word} => {players[i]}")
+        i += 1
+    players_short = players[i:] + players[:i]
+    players_short.pop(0)
+    counter(bytes_, players_short)
+    return players
 
+
+if __name__ == '__main__':
+    count = ["eny", "beny", "riky", "paky"]
+    name = list(range(7))
+    print(counter(count, name))
+
+    import hashlib
